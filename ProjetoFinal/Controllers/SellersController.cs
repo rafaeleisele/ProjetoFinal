7b28fb190b795af45ed350fc54361d2be9becf40
rafaeleisele <rafael_eisele@hotmail.com>
2022-01-27 10:35:51 -0300

    Returning custom error page

--- ProjetoFinal/Controllers/SellersController.cs
+++ ProjetoFinal/Controllers/SellersController.cs
@@ -7,6 +7,7 @@ using ProjetoFinal.Models;
 using ProjetoFinal.Services;
 using ProjetoFinal.Models.ViewModels;
 using ProjetoFinal.Services.Exceptions;
+using System.Diagnostics;
 
 namespace ProjetoFinal.Controllers
 {
@@ -47,13 +48,13 @@ namespace ProjetoFinal.Controllers
         {
             if (Id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(Id.Value);
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new {message = "Id not provided" });
             }
 
             return View(obj);
@@ -71,13 +72,13 @@ namespace ProjetoFinal.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
@@ -87,13 +88,13 @@ namespace ProjetoFinal.Controllers
         {
             if(id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             List<Department> departments = _departmentService.FindAll();
@@ -107,21 +108,31 @@ namespace ProjetoFinal.Controllers
         {
             if(id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
             try
             {
                 _sellerService.UpDate(seller);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (NotFoundException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
+            catch (DbConcurrencyException e)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
+        }
+
+        public IActionResult Error(string message)
+        {
+            var ViewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(ViewModel);
         }
     }
 }
--- ProjetoFinal/Models/ViewModels/ErrorViewModel.cs
+++ ProjetoFinal/Models/ViewModels/ErrorViewModel.cs
@@ -6,6 +6,7 @@ namespace ProjetoFinal.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
