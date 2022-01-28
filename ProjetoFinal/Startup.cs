736946b56c1518ff3a611587835ea6bb7eb74ccb
rafaeleisele <rafael_eisele@hotmail.com>
2022-01-28 13:14:59 -0300

    App locale, number and date formatting

--- ProjetoFinal/Startup.cs
+++ ProjetoFinal/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.EntityFrameworkCore;
 using ProjetoFinal.Models;
 using ProjetoFinal.Data;
 using ProjetoFinal.Services;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 namespace ProjetoFinal
 {
@@ -51,6 +53,16 @@ namespace ProjetoFinal
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
